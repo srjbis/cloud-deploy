e09fffdab52cad89c3d7689938e426a4e05bf175
srjbis007 <talk2surajbiswas@gmail.com>
2026-03-29 00:29:16 +0530

    Update main.tf

--- terraform-gke/main.tf
+++ terraform-gke/main.tf
@@ -1,13 +1,13 @@
 provider "google" {
   project = var.project_id
   region  = var.region
-  zone = var.zone
+  zone    = var.zone
 }
 
 module "gke" {
-  source     = "./modules/gke"
-  project_id = var.project_id
-  region     = var.region
-  zone = var.zone
+  source       = "./modules/gke"
+  project_id   = var.project_id
+  region       = var.region
+  zone         = var.zone
   cluster_name = "prod-gke"
 }
